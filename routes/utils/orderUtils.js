ebd766a1fb1aa6ceddc24707a10faf8dc72603a3
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-06-25 22:21:21 +0200

    ADD: get order From Db

--- routes/utils/orderUtils.js
+++ routes/utils/orderUtils.js
@@ -1,5 +1,6 @@
 const dbUtils = require('../db/dbUtils');
 
+/*module.exports.Order = */
 class Order {
     constructor(userID, order, comment, status) {
         this.userID = userID;
@@ -7,8 +8,25 @@ class Order {
         this.comment = comment;
         this.status = status;
     }
+
+    async postToDb(res, next) {
+        await dbUtils.query('INSERT INTO orders (userID, order, status, comment) VALUES (?, ?, ?, ?)', [this.userID, this.order, this.status, this.comment], res, next);
+    }
+
+    async getFromDb(ID) {
+        let order = await dbUtils.query('SELECT `order`, status, comment, transactions.ID, transactions.type, transactions.userID FROM orders INNER JOIN transactions ON orders.transactionID = transactions.ID WHERE orders.ID = ?', [ID]);
+        this.order = order.order;
+        this.comment = order.comment;
+        this.status = order.status;
+        this.userID = order.userID;
+
+        this.transaction = {
+            'ID': order.ID,
+            'type': order.type,
+            'userID': order.userID
+        }
+    }
 }
 
-function postOrderToDb(order, res, next) {
-    dbUtils.query('INSERT INTO orders (userID, order, status, comment) VALUES (?, ?, ?, ?)', [order.userID, order.order, order.status, order.comment], res, next);
-}
+let a = new Order();
+a.getFromDb(1)
