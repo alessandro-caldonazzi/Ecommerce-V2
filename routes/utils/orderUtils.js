f7de8511bf5c5c90b99159c68d866f7a3a5d0be5
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-06-25 22:28:38 +0200

    ALTER: (orderUtils) create transaction

--- routes/utils/orderUtils.js
+++ routes/utils/orderUtils.js
@@ -10,11 +10,12 @@ class Order {
     }
 
     async postToDb(res, next) {
+        await dbUtils.query('INSERT INTO transactions (type, userID) VALUES (?, ?)', [1, this.userID], res, next);
         await dbUtils.query('INSERT INTO orders (userID, order, status, comment) VALUES (?, ?, ?, ?)', [this.userID, this.order, this.status, this.comment], res, next);
     }
 
-    async getFromDb(ID) {
-        let order = await dbUtils.query('SELECT `order`, status, comment, transactions.ID, transactions.type, transactions.userID FROM orders INNER JOIN transactions ON orders.transactionID = transactions.ID WHERE orders.ID = ?', [ID]);
+    async getFromDb(ID, res, next) {
+        let order = await dbUtils.query('SELECT `order`, status, comment, transactions.ID, transactions.type, transactions.userID FROM orders INNER JOIN transactions ON orders.transactionID = transactions.ID WHERE orders.ID = ?', [ID], res, next);
         this.order = order.order;
         this.comment = order.comment;
         this.status = order.status;
@@ -27,6 +28,3 @@ class Order {
         }
     }
 }
-
-let a = new Order();
-a.getFromDb(1)
