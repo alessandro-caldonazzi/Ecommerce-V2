c2a0af90c4f9a4eea066d1cdd30af8946532f36d
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-06-29 10:17:23 +0200

    FIX: postToDb, getFromDb; ADD: connectTransaction

--- routes/utils/orderUtils.js
+++ routes/utils/orderUtils.js
@@ -10,22 +10,25 @@ class Order {
     }
 
     async postToDb(res, next) {
-        await dbUtils.query('INSERT INTO transactions (type, userID) VALUES (?, ?)', [1, this.userID], res, next);
-        let transactionID = await dbUtils.query('SELECT LAST_INSERT_ID()');
-        await dbUtils.query('INSERT INTO orders (userID, `order`, status, comment, transactionID) VALUES (?, ?, ?, ?, ?)', [this.userID, this.order, this.status, this.comment, transactionID[0]['LAST_INSERT_ID()']], res, next);
+        await dbUtils.query('INSERT INTO orders (userID, `order`, status, comment) VALUES (?, ?, ?, ?)', [this.userID, this.order, this.status, this.comment], res, next);
     }
 
     async getFromDb(ID, res, next) {
-        let order = await dbUtils.query('SELECT `order`, status, comment, transactions.ID, transactions.type, transactions.userID FROM orders INNER JOIN transactions ON orders.transactionID = transactions.ID WHERE orders.ID = ?', [ID], res, next);
+        let order = await dbUtils.query('SELECT `order`, status, comment, userID, transactionID FROM orders WHERE orders.ID = ?', [ID], res, next);
+        order = order[0];
         this.order = order.order;
         this.comment = order.comment;
         this.status = order.status;
         this.userID = order.userID;
 
-        this.transaction = {
-            'ID': order.ID,
-            'type': order.type,
-            'userID': order.userID
+        if (order.transactionID) {
+            order = await dbUtils.query('SELECT * FROM transactions WHERE ID = ?', [order.transactionID], res, next);
+            order = order[0];
+            this.transaction = {
+                'ID': order.ID,
+                'type': order.type,
+                'userID': order.userID
+            }
         }
     }
 
@@ -33,7 +36,11 @@ class Order {
         this.status = status;
         await db.query('UPDATE orders SET status = ? WHERE ID = ?', [status, this.ID], res, next);
     }
+
+    async connectTransaction(transactionID, res, next) {
+        await db.query('UPDATE orders SET transactionID = ? WHERE ID = ?', [transactionID, this.ID], res, next)
+    }
 }
 
-let a = new Order(3, "aa", "aaa", 1);
-a.postToDb()
+let a = new Order();
+a.getFromDb(4)
