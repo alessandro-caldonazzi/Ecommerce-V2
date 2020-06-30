238fe62171ba1555b8d44d954787b4b1be6f17a5
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-06-30 12:07:31 +0200

    NEW: changestatus endpoint under order

--- routes/order.js
+++ routes/order.js
@@ -18,10 +18,32 @@ router.post('/new', [
 
         let order = new Order(jwt.ID, userOrder, userComment, 1);
         await order.postToDb(res, next);
-
-        res.send({ 'success': true, 'data': { 'order': userOrder, 'comment': userComment, 'status': 1 } })
+        res.send({ 'success': true, 'data': { 'ID': order._ID, 'order': userOrder, 'comment': userComment, 'status': 1 } });
     } catch (err) {
-        console.log(err);
+        res.status(403).json();
+    }
+});
+
+router.post('/changestatus', [
+    check('ID').notEmpty().isLength({ min: 1, max: 10 }),
+    check('status').notEmpty().matches('[0-9]').isLength({ min: 1, max: 1 })
+], async(req, res, next) => {
+    try {
+        validationResult(req).throw();
+        const jwt = req.jwt;
+        const ID = Number.parseInt(req.body.ID);
+        const status = Number.parseInt(req.body.status);
+
+        if (jwt.rank < 1) throw 'Invalid Permission';
+        if (Number.isNaN(ID) || Number.isNaN(status)) throw 'Invalid ID or status';
+
+        let order = new Order();
+        await order.getFromDb(ID, res, next);
+        await order.changeStatus(status);
+
+        res.send({ 'success': true });
+    } catch (err) {
+        console.log(err)
         res.status(403).json();
     }
 });
